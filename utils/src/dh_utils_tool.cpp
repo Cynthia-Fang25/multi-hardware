90d196040640b94a89b7498388ad4dbda0e0b358
li-tiangang4 <litiangang4@huawei.com>
2022-08-12 10:40:54 +0800

    修改
    Signed-off-by: li-tiangang4 <litiangang4@huawei.com>

--- utils/src/dh_utils_tool.cpp
+++ utils/src/dh_utils_tool.cpp
@@ -116,7 +116,6 @@ DeviceInfo GetLocalDeviceInfo()
         DHLOGE("GetLocalNodeDeviceInfo failed, errCode = %d", ret);
         return devInfo;
     }
-
     devInfo.uuid = GetUUIDBySoftBus(info->networkId);
     devInfo.deviceId = GetDeviceIdByUUID(devInfo.uuid);
     devInfo.deviceName = info->deviceName;
