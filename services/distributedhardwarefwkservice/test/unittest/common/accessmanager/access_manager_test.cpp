3dd5c88fb875b46bc4d1ae508319903b25ac0d00
wangyb0625 <wangyibo38@huawei.com>
2022-09-29 09:04:55 +0800

    ut重复去初始化
    Signed-off-by: wangyb0625 <wangyibo38@huawei.com>

--- services/distributedhardwarefwkservice/test/unittest/common/accessmanager/access_manager_test.cpp
+++ services/distributedhardwarefwkservice/test/unittest/common/accessmanager/access_manager_test.cpp
@@ -236,16 +236,5 @@ HWTEST_F(AccessManagerTest, AccessManagerInit, TestSize.Level0)
 {
     EXPECT_EQ(DH_FWK_SUCCESS, AccessManager::GetInstance()->Init());
 }
-
-/**
- * @tc.name: AccessManagerUnInit
- * @tc.desc: Verify the  Init UnInit function
- * @tc.type: FUNC
- * @tc.require: AR000GHSJM
- */
-HWTEST_F(AccessManagerTest, AccessManagerUnInit, TestSize.Level0)
-{
-    EXPECT_EQ(DH_FWK_SUCCESS, AccessManager::GetInstance()->UnInit());
-}
 } // namespace DistributedHardware
 } // namespace OHOS
