233e75ef00751a7bd16a9ae6dc7221d66c29dc49
chen0088 <chenmenghuan6@huawei.com>
2024-07-27 14:09:43 +0800

    modify
    Signed-off-by: chen0088 <chenmenghuan6@huawei.com>

--- av_transport/av_trans_engine/plugin/plugins/av_trans_input/dsoftbus_input/dsoftbus_input_plugin.cpp
+++ av_transport/av_trans_engine/plugin/plugins/av_trans_input/dsoftbus_input/dsoftbus_input_plugin.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Copyright (c) 2023-2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -295,7 +295,7 @@ std::shared_ptr<Buffer> DsoftbusInputPlugin::CreateBuffer(uint32_t metaType,
         return buffer;
     }
     cJSON *paramItem = cJSON_GetObjectItem(resMsg, AVT_DATA_PARAM.c_str());
-    if (paramItem == NULL) {
+    if (paramItem == NULL || !cJSON_IsString(paramItem)) {
         return nullptr;
     }
     auto meta = std::make_shared<AVTransVideoBufferMeta>();
