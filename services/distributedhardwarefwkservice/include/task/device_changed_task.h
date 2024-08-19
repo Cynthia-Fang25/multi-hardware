ab5a1aa7f8800dfebc336287f50429f1e160ef33
saga <donghuibin@huawei.com>
2024-08-19 13:09:42 +0000

    update services/distributedhardwarefwkservice/include/task/device_changed_task.h.
    Signed-off-by: saga <donghuibin@huawei.com>

--- services/distributedhardwarefwkservice/include/task/device_changed_task.h
+++ services/distributedhardwarefwkservice/include/task/device_changed_task.h
@@ -23,7 +23,7 @@ namespace DistributedHardware {
 class DeviceChangedTask : public Task {
 public:
     DeviceChangedTask() = delete;
-    DeviceChangedTask(const std::string &networkId, const std::string &uuid,
+    DeviceChangedTask(const std::string &networkId, const std::string &uuid, const std::string &udid,
         const std::string &dhId, const DHType dhType);
     virtual ~DeviceChangedTask();
     virtual void DoTask();
